630a9bffc42a86401187531f9aacffd0858e6430
swapon789 <kumarshopon850@gmail.com>
2022-12-28 21:42:47 +0600

    Home and footer section added

---
+++ src/Components/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainFrame from '../Navbar/MainFrame/MainFrame';
+
+
+const Home = () => {
+    return (
+        <div>
+            <MainFrame></MainFrame>
+        </div>
+    );
+};
+
+export default Home;
