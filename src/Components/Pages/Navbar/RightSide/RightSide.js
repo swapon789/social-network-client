6bfe23ee45464b932170defef33a436a6fbf68b8
swapon789 <kumarshopon850@gmail.com>
2022-12-28 22:03:52 +0600

    rightside  section added

---
+++ src/Components/Pages/Navbar/RightSide/RightSide.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+
+const RightSide = () => {
+    return (
+        <section className="py-6 hidden md:block dark:bg-gray-800 dark:text-gray-100">
+
+            <div className="flex flex-col justify-center">
+                <div className='flex items-center justify-center gap-2 mb-10'>
+                    <h2 className='font-bold mr-4 text-2xl'>Contact</h2>
+                </div>
+                <div className="flex flex-row flex-wrap-reverse  justify-center m-2 text-center">
+                    <img alt="" className="self-center  flex-shrink-0 w-8 h-8 mb-2 bg-center bg-cover rounded-full dark:bg-gray-500" src="https://source.unsplash.com/100x100/?portrait?0" />
+                    <p className="text-sm font-semibold leading-tight mx-2 mt-1">Sanjib Halder</p>
+                </div>
+                <div className="flex flex-row flex-wrap-reverse  justify-center m-2 text-center">
+                    <img alt="" className="self-center flex-shrink-0 w-8 h-8 mb-2 bg-center bg-cover rounded-full dark:bg-gray-500" src="https://source.unsplash.com/100x100/?portrait?0" />
+                    <p className="text-sm font-semibold leading-tight mx-2 mt-1">Linkon Mitro</p>
+                </div>
+                <div className="flex flex-row flex-wrap-reverse  justify-center m-2 text-center">
+                    <img alt="" className="self-center flex-shrink-0 w-8 h-8 mb-2 bg-center bg-cover rounded-full dark:bg-gray-500" src="https://source.unsplash.com/100x100/?portrait?0" />
+                    <p className="text-sm font-semibold leading-tight mx-2 mt-1">Pitter Champ</p>
+                </div>
+                <div className="flex flex-row flex-wrap-reverse  justify-center m-2 text-center">
+                    <img alt="" className="self-center flex-shrink-0 w-8 h-8 mb-2 bg-center bg-cover rounded-full dark:bg-gray-500" src="https://source.unsplash.com/100x100/?portrait?0" />
+                    <p className="text-sm font-semibold leading-tight mx-2 mt-1">Naila Nayem</p>
+                </div>
+                <div className="flex flex-row flex-wrap-reverse  justify-center m-2 text-center">
+                    <img alt="" className="self-center flex-shrink-0 w-8 h-8 mb-2 bg-center bg-cover rounded-full dark:bg-gray-500" src="https://source.unsplash.com/100x100/?portrait?0" />
+                    <p className="text-sm font-semibold leading-tight mx-2 mt-1">Milton Robin</p>
+                </div>
+                <div className="flex flex-row flex-wrap-reverse  justify-center m-2 text-center">
+                    <img alt="" className="self-center flex-shrink-0 w-8 h-8 mb-2 bg-center bg-cover rounded-full dark:bg-gray-500" src="https://source.unsplash.com/100x100/?portrait?0" />
+                    <p className="text-sm font-semibold leading-tight mx-2 mt-1">Sonali Jesia</p>
+                </div>
+            </div>
+
+        </section>
+    );
+};
+
+export default RightSide;
