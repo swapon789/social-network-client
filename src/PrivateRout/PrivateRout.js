f784d148d75bca29c8b8dfee0ddde5364277a780
swapon789 <kumarshopon850@gmail.com>
2022-12-28 21:12:42 +0600

    node install and install tailwind css and some frame work and dom

---
+++ src/PrivateRout/PrivateRout.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { AuthContext } from '../../Context/AuthProvider/AuthProvider';
+
+const PrivateRout = ({children}) => {
+    const {user,} = useContext(AuthContext);
+    const location = useLocation();
+    
+   
+    if(!user){
+        return <Navigate to='/login' state={{from:location}} replace></Navigate>
+    }
+    return children;
+};
+
+export default PrivateRout;
